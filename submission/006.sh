763db510a828f944e14aa290478db5dd5a42d087
lgustavovg <89117121+lgustavovg@users.noreply.github.com>
2025-02-02 03:18:00 -0300

    006.sh

--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,23 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
+
+# Bloco 256,128
+hash256128=$(bitcoin-cli getblockhash 256128)
+tx_cb=$(bitcoin-cli getblock $hash256128 | jq -r '.tx[0]') # A transação coinbase geralmente é a primeira do bloco
+
+# Bloco 257,343
+hash257343=$(bitcoin-cli getblockhash 257343)
+txblock=$(bitcoin-cli getblock $hash257343 | jq -r '.tx[]')
+
+for txid in $txblock; do
+    
+    clean_txid=$(echo "$txid" | tr -d '[:space:]') # Remover espaços e caracteres extras do txid
+    inputs=$(bitcoin-cli getrawtransaction "$clean_txid" 1 | jq -r '.vin[].txid')
+
+    for input in $inputs; do
+        clean_input=$(echo "$input" | tr -d '[:space:]')  # Remover espaços e caracteres extras do input
+        if [[ "$clean_input" == "$tx_cb" ]]; then
+            echo $clean_txid
+            exit 0
+        fi
+    done
+done
